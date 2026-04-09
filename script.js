1f256a45ff03fd87ab913f0e806c00495a537753
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-04-09 11:18:17 -0300

    ADD: Inicialização do JS com as funções

---
+++ script.js
@@ -0,0 +1,27 @@
+const colorPicker = document.querySelector('#color-picker');
+const fonte = document.querySelector('#font-select');
+const colorArtigos = document.querySelector('#article-color-picker');
+const themeToggle = document.querySelector('#theme-toggle');
+const body = document.querySelector('body');
+const newsArticles = document.querySelector('.news');
+
+
+// verifica o localstorage com as preferencias do usuario
+const loadPreferences = () =>{
+}
+
+colorPicker.addEventListener('input', (e) => {
+    const color = e.target.value;
+    document.documentElement.style.setProperty('--primary-color', color);
+    localStorage.setItem('primaryColor', color);
+});
+
+colorArtigos.addEventListener('input', (e) =>{
+});
+
+fonte.addEventListener('change', (e) =>{
+});
+
+themeToggle.addEventListener('click', () =>{
+});
+
