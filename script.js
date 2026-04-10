a3235b1555b4463bd6f881b9e8135e02e4698706
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-04-10 09:01:01 -0300

    Funções

--- script.js
+++ script.js
@@ -6,29 +6,67 @@ const body = document.querySelector('body');
 const newsArticles = document.querySelector('.news');
 
 
-// verifica o localstorage com as preferencias do usuario
+// verifica o localstorage com as preferencias do usuario (utilizando if's)
 const loadPreferences = () =>{
+    // Cor
+    const savedColor = localStorage.getItem('primaryColor');
+    if (savedColor){
+        document.documentElement.style.setProperty('--primary-color', savedColor);
+        colorPicker.value = savedColor;
+    }
+
+    // Cor dos Artigos 
+    const savedArticleColor = localStorage.getItem('articleColor');
+    if (savedArticleColor){
+        document.documentElement.style.setProperty('--bg-color', savedArticleColor);
+        colorArtigos.value = savedArticleColor;
+    }
+
+    // Fonte
+    const savedFont = localStorage.getItem('font-family');
+    if (savedFont){
+        document.documentElement.style.setProperty('--font-family', savedFont);
+        fonte.value = savedFont;
+    }
+
+    // Tema
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark'){
+        body.classList.add('dark')
+        themeToggle.innerHTML = '<span class="material-icons">dark_mode</span>';
+    }
+
 }
 
 colorPicker.addEventListener('input', (e) => {
     const color = e.target.value;
-    console.log(color)
     document.documentElement.style.setProperty('--primary-color', color);
     localStorage.setItem('primaryColor', color);
 });
 
-colorArtigos.addEventListener('input', (e) =>{
+colorArtigos.addEventListener('input', (e) => {
     const color = e.target.value;
     newsArticles.forEach(article => {article.style.backgroundColor = color;});
     localStorage.setItem('articleColor', color);
 
 });
 
-fonte.addEventListener('change', (e) =>{
+fonte.addEventListener('change', (e) => {
     const fonte = e.target.value;
-    document.documentElement
+    console.log(fonte);
+    document.documentElement.style.setProperty('--font-family', fonte);
+    localStorage.setItem('fontFamily', fonte);
 });
 
 themeToggle.addEventListener('click', () =>{
+    body.classList.toggle('dark');
+    const isDark = body.classList.contains('dark');
+
+    themeToggle.innerHTML = isDark
+    ? '<span class="material-icons">light_mode</span>'
+    : '<span class="material-icons">dark_mode</span>';
+
+    localStorage.setItem('theme', isDark ? 'dark' : 'light')
 });
 
+loadPreferences();
