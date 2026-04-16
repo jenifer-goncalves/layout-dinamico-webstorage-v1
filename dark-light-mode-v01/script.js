1f43001a6971c059ae8add2764dbcfe17d4fbe36
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-04-16 11:11:49 -0300

    ADD: Inicio do código em JS

--- dark-light-mode-v01/script.js
+++ dark-light-mode-v01/script.js
@@ -1,17 +1,28 @@
 // Evento que é disparado quando o conteúdo do DOM (estrutura HTML) é totalmente carregado
+document.addEventListener("DOMContentLoaded", function(){
+    
+});
 
+// Verifica no localStorage se existe um tema salvo. Se não houver, define o tema padrão como 'light'
+let temaSalvo = localStorage.getItem('tema');
 
-  // Verifica no localStorage se existe um tema salvo. Se não houver, define o tema padrão como 'light'
- 
+if (!temaSalvo){
+    temaSalvo = 'light';
+ }
 
-  // Adiciona a classe correspondente ao tema (light ou dark) no corpo da página (body)
+// Adiciona a classe correspondente ao tema (light ou dark) no corpo da página (body)
+document.body.classList.add(temaSalvo);
 
+// Seleciona o botão de alternar tema usando o ID 'toggle-theme' e adiciona um evento de clique
+  const btn = document.querySelector("#toggle-theme");
 
-  // Seleciona o botão de alternar tema usando o ID 'toggle-theme' e adiciona um evento de clique
-  
+    btn.addEventListener("click", function(){
+        console.log("Verifica se clica")
+    });
     
     // Verifica qual tema está atualmente aplicado no body (light ou dark)
     // Se o tema for 'light', muda para 'dark', caso contrário, muda para 'light'
+
     
 
     // Remove as classes de tema antigas ('light' e 'dark') para evitar conflito
