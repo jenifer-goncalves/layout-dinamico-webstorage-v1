7981abf606bfdf79e05eedbbac5022967d701286
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-04-23 11:15:53 -0300

    Inserção do js do menu e do main

--- spa-fetch-components/components/menu.js
+++ spa-fetch-components/components/menu.js
@@ -1,35 +1,36 @@
-const criarMenu = (menu) =>{
+export const iniciarMenu = () =>{
+    
+    const menus = document.querySelectorAll('[data-menu]');
 
-    const botao = menu.querySelector('.menu__botao');
+    menus.forEach((menu) => {
 
-    const toggleMenu = () => {
+        const botao = menu.querySelector('.menu__botao');
+        
+        const toggleMenu = () => {
+            menu.classList.toggle('active');
+            const ativo = menu.classList.contains('active');
+            botao.setAttribute('aria-expanded', ativo);
 
-        menu.classList.toggle('active');
-        const ativo = menu.classList.contains('active');
-        botao.setAttribute('aria-expanded', ativo);
+            botao.setAttribute(
+                'aria-label',
+                ativo ? 'Fechar Menu' : 'Abrir Menu'
+            );
+        };
 
-        botao.setAttribute(
-            'aria-label',
-            ativo ? 'Fechar Menu' : 'Abrir Menu'
-        );
-    };
+        const fecharComEsc = (event) => {
+            const isEsc = event.key === 'Escape';
+            const ativo = menu.classList.contains('active');
 
-    const fecharComEsc = (event) => {
-        const isEsc = event.key === 'Escape';
-        const ativo = menu.classList.contains('active');
-
-        if (isEsc && ativo){
-            menu.classList.remove('active');
-            botao.setAttribute('aria-expanded', false);
-            botao.setAttribute('aria-label', 'Abrir Menu');
-            botao.focus();
-        }
-    };
-
-    botao.addEventListener('click', toggleMenu);
-    document.addEventListener('keydown', fecharComEsc)
-
-
+            if (isEsc && ativo){
+                menu.classList.remove('active');
+                botao.setAttribute('aria-expanded', false);
+                botao.setAttribute('aria-label', 'Abrir Menu');
+                botao.focus();
+            }
+        };
+    
+        botao.addEventListener('pointerdown', toggleMenu);
+        document.addEventListener('keydown', fecharComEsc)
+        
+    });
 };
-
-document.querySelectorAll('[data-menu]').forEach(menu => criarMenu(menu));
