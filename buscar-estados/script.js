c81f522c64d7d1a4207da8bd41c75c42dc9401ad
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-05-07 11:28:19 -0300

    ADD: Inicialização do JS com fetch

--- buscar-estados/script.js
+++ buscar-estados/script.js
@@ -1,1 +1,23 @@
+const estado = document.querySelector('#estados')
+const municipios = document.querySelector('#municipios')
 
+estado.addEventListener('click', async () => {
+    const est = estado.value
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${est}/municipios`
+    fetch(url)
+    // Até aqui acima ^ correto
+async function buscarMunicipio(){
+    try{
+        let resposta = await fetch(url);
+        if (!resposta.ok){
+            throw new Error('Erro na requisição')
+        }
+        let municipios = await resposta.json();
+        for (let)
+
+    }catch(error){
+        console.warn(error.message)
+    }
+};
+
+});
